2f22961df9bc4ea63fee900f8079ca176f2d9364
SamuelRivasM <samuelrivasm@gmail.com>
2026-05-22 04:21:42 -0500

    Cambio en la conexion para conectarse a TiDB Cloud

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,17 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'stay_event',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
@@ -15,10 +21,13 @@ const pool = mysql.createPool({
 const testConnection = async () => {
     try {
         const conn = await pool.getConnection();
-        console.log('Conectado a MySQL (XAMPP)');
+
+        console.log('Conectado a TiDB Cloud');
+
         conn.release();
     } catch (error) {
         console.error('Error de conexión a MySQL:', error.message);
+
         process.exit(1);
     }
 };
